5e2af5b3f0149fbeacc9a09486c5025d7bfbe53c
walkingice0204 <walkingice0204@bc56f300-e732-11dd-9d8c-a70aa0a93a20>
2009-01-27 13:32:25 +0000

    This patch attempts to fix the improper intltool and autotool handling against Dindin PDF.
    *The author of this patch is Jim Huang(a.k.a Jserv) <http://jserv.sayya.org/>
     Thanks, Jserv.

--- autogen.sh
+++ autogen.sh
@@ -10,7 +10,7 @@ echo "Running autopoint..." ; autopoint -f || :
 echo "Running aclocal..." ; aclocal -I m4 $ACLOCAL_FLAGS || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
-echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
+echo "Running intltoolize..." ; (intltoolize -c --automake --force) || exit 1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
